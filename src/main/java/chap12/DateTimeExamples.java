b1ebdad564d2762043e92e1cf04245d668341d66
woorim05 <84488846+woorim05@users.noreply.github.com>
2024-04-22 15:52:45 +0900

    study chap12. 날짜와 시간 API

---
+++ src/main/java/chap12/DateTimeExamples.java
@@ -0,0 +1,62 @@
+package chap12;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class DateTimeExamples {
+    public static void main(String[] args) {
+        // 날짜
+        LocalDate today = LocalDate.now();          // 오늘 날짜(YYYY-MM-dd)
+        LocalDate dateFrom = LocalDate.from(today); // 오늘 날짜(YYYY-MM-dd)
+        LocalDate date = LocalDate.of(2024, 4, 19); // 2024-04-19
+        int year = date.getYear();                  // 2024
+        Month month = date.getMonth();              // APRIL
+        int day = date.getDayOfMonth();             // 19
+        DayOfWeek dow = date.getDayOfWeek();        // FRIDAY
+        int len = date.lengthOfMonth();             // 30 (월의 일수)
+        boolean leap = date.isLeapYear();           // true(윤년여부)
+
+        // 시간
+        LocalTime time = LocalTime.of(13, 45, 20);  // 13:45:20
+        int hour = time.getHour();                  // 13
+        int minute = time.getMinute();              // 45
+        int second = time.getSecond();              // 20
+
+        // 날짜 시간 한번에
+        LocalDateTime dt1 = LocalDateTime.of(2024, Month.APRIL, 19, 13, 45, 20); // 2024-04-19T13:45:20
+        LocalDateTime dt2 = LocalDateTime.of(date, time); // 2024-04-19T13:45:20
+        LocalDateTime dt3 = time.atDate(date); // 2024-04-19T13:45:20
+        LocalDateTime dt4 = date.atTime(13, 45, 20); // 2024-04-19T13:45:20
+        LocalDateTime dt5 = date.atTime(time); // 2024-04-19T13:45:20
+
+        LocalDate date1 = dt1.toLocalDate(); // 2024-04-19
+        LocalTime time1 = dt1.toLocalTime(); // 13:45:20
+
+        // 날짜 조정
+        date1 = date1.plusWeeks(1); // 2024-04-26
+        date1 = date1.minusYears(2); // 2022-04-26
+        date1 = date1.plus(3, ChronoUnit.MONTHS); //  2022-07-26
+
+        LocalDate date2 = date1.withYear(2024);     // 2024-07-26
+        LocalDate date3 = date2.withMonth(4);       // 2024-04-26
+        LocalDate date4 = date3.withDayOfMonth(19); // 2024-04-19
+
+
+        // 아래 Instant는 모두 같음
+        Instant.ofEpochSecond(3);
+        Instant.ofEpochSecond(3, 0);
+        Instant.ofEpochSecond(2, 1000000000);  // 10억초 = 1초
+        Instant.ofEpochSecond(4, -1000000000);
+
+        // format & parse
+        String s1 = date.format(DateTimeFormatter.BASIC_ISO_DATE); // 20240419
+        String s2 = date.format(DateTimeFormatter.ISO_LOCAL_DATE); // 2024-04-19
+        LocalDate date5 = LocalDate.parse("20240422", DateTimeFormatter.BASIC_ISO_DATE); // 20240422
+        LocalDate date6 = LocalDate.parse("2024-04-22", DateTimeFormatter.ISO_LOCAL_DATE); // 2024-04-22
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formattedDate = date5.format(formatter); // 22/04/2024
+        LocalDate date7 = LocalDate.parse(formattedDate, formatter); // 2024-04-22
+    }
+}
---
+++ src/main/java/chap12/README.md
@@ -0,0 +1,82 @@
+# 날짜와 시간 API
+
+---
+### 1. LocalDate, LocalTime 클래스
+- 날짜와 시간 생성 및 읽기
+  - from(Temporal): 주어진 Temporal로 인스턴스 생성
+  - now: 시스템 시계로 인스턴스 생성
+  - of: 주어진 구성 요소로 인스턴스 생성
+  - parse: 문자열을 파싱해서 생성
+  - get: Temproal 읽음
+- 날짜 조정, 포매팅
+  - format: [지정된 포매터(DateTimeFormatter)](#5.-DateTimeFormatter)를 이용해서 문자열로 변환
+  - minus
+  - plus
+  - with
+
+### 2. Instant 클래스
+- 기계의 날짜와 시간
+- 유닉스 에포크 시간(1970년 1월 1일 0시 0분 0초 UTC)을 기준으로 특정 지점까지의 시간을 초단위로 표현
+- 나노초(10억분의 1초)의 정밀도를 제공
+- ofEpochSecond() 로 Instant 생성
+  - 첫 번째 인수: 구하고자 하는 초
+  - 두 번째 인수: 수정하고 싶은 만큼의 나노초
+
+### 3. Duration, Period
+- Duration 클래스는 초/나노초로 시간 단위를 표현하므로 LocalTime, LocalDateTime, Instant 로만 비교 할 수 있음
+- LocalDate를 사용하고 싶을 때는 Period 클래스 사용
+- Duration & Period 공통 메서드
+  - between: 두 시간 사이의 간격을 생성
+  - from: 시간 단위로 간격을 생성
+  - of: 주어진 구성 요소(amount)에서 간격 인스턴스를 생성
+  - parse: 문자열 파싱해서 간격 인스턴ㅣㅣㅣㅣㅣ스 생성
+  - addTo: 복사본 생성 후 지정된 temporal 객체에 추가
+  - get: 현재 간격 정보 읽음
+  - isNegative: 간격이 음수인지 확인
+  - isZero: 간격이 0인지 확인
+  - minus: 주어진 시간을 뺀 복사본을 생성
+  - multipliedBy: 주어진 값을 곱한 복사본 생성
+  - negated: 주어진 값으 부호를 반전한 복사본 생성
+  - plus: 주어진 시간을 더한 복사본을 생성
+  - subtractFrom: 지정된 Temporal 객체에서 간격을 뺌
+  
+### 4. TemporalAdjusters
+- 날짜 조정 기능 클래스
+  - dayOfWeekInMonth
+  - firstDayOfMonth
+  - firstDayOfNextMonth
+  - firstDayOfNextYear
+  - firstDayOfYear
+  - firstInMonth
+  - lastDayOfMonth
+  - lastDayOfNextMonth
+  - lastDayOfNextYear
+  - lastDayOfYear
+  - lastInMonth
+  - next
+  - previous
+  - nextOrSame
+  - previousOrSame
+  
+### 5. DateTimeFormatter
+- 정의된 상수 사용하기
+  - ***ISO_LOCAL_DATE(YYYY-MM-dd)***
+  - ISO_OFFSET_DATE
+  - ISO_DATE
+  - ISO_LOCAL_TIME
+  - ISO_OFFSET_TIME
+  - ISO_TIME
+  - ISO_LOCAL_DATE_TIME
+  - ISO_OFFSET_DATE_TIME
+  - ISO_ZONED_DATE_TIME
+  - ISO_DATE_TIME
+  - ISO_ORDINAL_DATE
+  - ISO_WEEK_DATE
+  - ISO_INSTANT
+  - ***BASIC_ISO_DATE(YYYYMMdd)***
+  - RFC_1123_DATE_TIME
+- 메서드
+  - ofPattern
+- 세부적인 포매터 제어
+  - new DateTimeFormatterBuilder()
+  
